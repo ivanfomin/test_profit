71c64b6deec8e179faf702da939f6c72b93b6ff2
Ivan Fomin <la-verne@mail.ru>
2019-12-02 10:46:17 +0300

    homeWork1.2

--- 1/Calculator/calculate_test.php
+++ 1/Calculator/calculate_test.php
@@ -29,3 +29,92 @@ if ($res == 0) {
     echo 'OK' . PHP_EOL;
 }
 
+//Negative
+
+$res = calc('abc', 0, '/');
+
+if ($res == 0) {
+    echo 'OK' . PHP_EOL;
+}
+
+$res = calc('10', 'abc', '/');
+
+if ($res == 0) {
+    echo 'OK' . PHP_EOL;
+}
+
+$res = calc(INF, 1, '/');
+
+if ($res == INF) {
+    echo 'OK' . PHP_EOL;
+}
+
+$res = calc(1, INF, '*');
+
+if ($res == INF) {
+    echo 'OK' . PHP_EOL;
+}
+
+$res = calc(100, 100, '*');
+
+if ($res < 10000) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '+');
+
+if ($res < 200) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '/');
+
+if ($res < 1) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '-');
+
+if ($res < 0) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '*');
+
+if ($res > 10000) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '+');
+
+if ($res > 200) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '/');
+
+if ($res > 1) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
+
+$res = calc(100, 100, '-');
+
+if ($res > 0) {
+    echo 'Not Pass' . PHP_EOL;
+} else {
+    echo 'Pass' . PHP_EOL;
+}
